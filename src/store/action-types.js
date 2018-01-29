ca65fc45b805067f6c8db6257b6e37f4abc729fe
zjqzlyzjq <2193542487@qq.com>
2018-01-29 12:16:47 +0800

    项目结构

---
+++ src/store/action-types.js
@@ -0,0 +1,13 @@
+export const SET_LESSON = 'SET_LESSON';
+
+export const SET_SLIDERS = 'SET_SLIDERS';
+
+export const SET_LESSONS = 'SET_LESSONS';
+
+export const CHANGE_LOADING = 'CHANGE_LOADING';
+
+export const CLEAR_LESSONS = 'CLEAR_LESSONS';
+
+export const SET_USER_INFO = 'SET_USER_INFO';
+
+export const CLEAR_MSG = 'CLEAR_MSG';
