65d683d2b4725387a7e233d6ef940d55910d97c1
Jtrust <719284178@qq.com>
2018-01-30 19:21:35 +0800

    页面样式

--- src/containers/movie/Movie.js
+++ src/containers/movie/Movie.js
@@ -1,12 +1,167 @@
-import React from  'react';
+import React from 'react';
 import './index.less'
 
-export default class Movie extends  React.Component{
-    render(){
+export default class Movie extends React.Component {
+    render() {
         return <div>
-            <div className="box">
-                <h2></h2>
-            </div>
+            <section>
+                <div className="title-box">
+                    <h2>影院热映</h2>
+                    <a href="">更多</a>
+                </div>
+                <div className="list-box">
+                    <div className="items">
+                        <a href="">
+                            <img src="https://img3.doubanio.com/view/photo/m_ratio_poster/public/p2508618114.jpg" alt=""/>
+                            <span className="item-title">移动迷宫3：死亡解药22222</span>
+                            <div className="item-rating">
+                                <div className="rank">
+                                <span className="rating-stars" data-rating="2.7">
+                                    <span className="rating-star rating-star-small-full"></span><span
+                                    className="rating-star rating-star-small-full"></span><span
+                                    className="rating-star rating-star-small-full"></span><span
+                                    className="rating-star rating-star-small-gray"></span><span
+                                    className="rating-star rating-star-small-gray"></span>
+                                </span>
+                                    <span>5.4</span>
+                                </div>
+                            </div>
+
+                        </a>
+                        <a href="">
+                            <img src="https://img3.doubanio.com/view/photo/m_ratio_poster/public/p2508618114.jpg" alt=""/>
+                            <span className="item-title">移动迷宫3：死亡解药22222</span>
+                            <div className="item-rating">
+                                <div className="rank">
+                                <span className="rating-stars" data-rating="2.7">
+                                    <span className="rating-star rating-star-small-full"></span><span
+                                    className="rating-star rating-star-small-full"></span><span
+                                    className="rating-star rating-star-small-full"></span><span
+                                    className="rating-star rating-star-small-gray"></span><span
+                                    className="rating-star rating-star-small-gray"></span>
+                                </span>
+                                    <span>5.4</span>
+                                </div>
+                            </div>
+
+                        </a>
+                        <a href="">
+                            <img src="https://img3.doubanio.com/view/photo/m_ratio_poster/public/p2508618114.jpg" alt=""/>
+                            <span className="item-title">移动迷宫3：死亡解药22222</span>
+                            <div className="item-rating">
+                                <div className="rank">
+                                <span className="rating-stars" data-rating="2.7">
+                                    <span className="rating-star rating-star-small-full"></span><span
+                                    className="rating-star rating-star-small-full"></span><span
+                                    className="rating-star rating-star-small-full"></span><span
+                                    className="rating-star rating-star-small-gray"></span><span
+                                    className="rating-star rating-star-small-gray"></span>
+                                </span>
+                                    <span>5.4</span>
+                                </div>
+                            </div>
+
+                        </a>
+                        <a href="">
+                            <img src="https://img3.doubanio.com/view/photo/m_ratio_poster/public/p2508618114.jpg" alt=""/>
+                            <span className="item-title">移动迷宫3：死亡解药22222</span>
+                            <div className="item-rating">
+                                <div className="rank">
+                                <span className="rating-stars" data-rating="2.7">
+                                    <span className="rating-star rating-star-small-full"></span><span
+                                    className="rating-star rating-star-small-full"></span><span
+                                    className="rating-star rating-star-small-full"></span><span
+                                    className="rating-star rating-star-small-gray"></span><span
+                                    className="rating-star rating-star-small-gray"></span>
+                                </span>
+                                    <span>5.4</span>
+                                </div>
+                            </div>
+
+                        </a>
+                    </div>
+
+                </div>
+            </section>
+            <section>
+                <div className="title-box">
+                    <h2>影院热映</h2>
+                    <a href="">更多</a>
+                </div>
+                <div className="list-box">
+                    <div className="items">
+                        <a href="">
+                            <img src="https://img3.doubanio.com/view/photo/m_ratio_poster/public/p2508618114.jpg" alt=""/>
+                            <span className="item-title">移动迷宫3：死亡解药22222</span>
+                            <div className="item-rating">
+                                <div className="rank">
+                                <span className="rating-stars" data-rating="2.7">
+                                    <span className="rating-star rating-star-small-full"></span><span
+                                    className="rating-star rating-star-small-full"></span><span
+                                    className="rating-star rating-star-small-full"></span><span
+                                    className="rating-star rating-star-small-gray"></span><span
+                                    className="rating-star rating-star-small-gray"></span>
+                                </span>
+                                    <span>5.4</span>
+                                </div>
+                            </div>
+
+                        </a>
+                        <a href="">
+                            <img src="https://img3.doubanio.com/view/photo/m_ratio_poster/public/p2508618114.jpg" alt=""/>
+                            <span className="item-title">移动迷宫3：死亡解药22222</span>
+                            <div className="item-rating">
+                                <div className="rank">
+                                <span className="rating-stars" data-rating="2.7">
+                                    <span className="rating-star rating-star-small-full"></span><span
+                                    className="rating-star rating-star-small-full"></span><span
+                                    className="rating-star rating-star-small-full"></span><span
+                                    className="rating-star rating-star-small-gray"></span><span
+                                    className="rating-star rating-star-small-gray"></span>
+                                </span>
+                                    <span>5.4</span>
+                                </div>
+                            </div>
+
+                        </a>
+                        <a href="">
+                            <img src="https://img3.doubanio.com/view/photo/m_ratio_poster/public/p2508618114.jpg" alt=""/>
+                            <span className="item-title">移动迷宫3：死亡解药22222</span>
+                            <div className="item-rating">
+                                <div className="rank">
+                                <span className="rating-stars" data-rating="2.7">
+                                    <span className="rating-star rating-star-small-full"></span><span
+                                    className="rating-star rating-star-small-full"></span><span
+                                    className="rating-star rating-star-small-full"></span><span
+                                    className="rating-star rating-star-small-gray"></span><span
+                                    className="rating-star rating-star-small-gray"></span>
+                                </span>
+                                    <span>5.4</span>
+                                </div>
+                            </div>
+
+                        </a>
+                        <a href="">
+                            <img src="https://img3.doubanio.com/view/photo/m_ratio_poster/public/p2508618114.jpg" alt=""/>
+                            <span className="item-title">移动迷宫3：死亡解药22222</span>
+                            <div className="item-rating">
+                                <div className="rank">
+                                <span className="rating-stars" data-rating="2.7">
+                                    <span className="rating-star rating-star-small-full"></span><span
+                                    className="rating-star rating-star-small-full"></span><span
+                                    className="rating-star rating-star-small-full"></span><span
+                                    className="rating-star rating-star-small-gray"></span><span
+                                    className="rating-star rating-star-small-gray"></span>
+                                </span>
+                                    <span>5.4</span>
+                                </div>
+                            </div>
+
+                        </a>
+                    </div>
+
+                </div>
+            </section>
         </div>
     }
 }
