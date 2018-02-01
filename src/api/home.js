c186452bbaa6b1928c325bd31bbe82717fab17b9
zjqzlyzjq <2193542487@qq.com>
2018-02-01 19:31:19 +0800

    解决冲突

--- src/api/home.js
+++ src/api/home.js
@@ -1,21 +1,13 @@
 import axios from './index';
 
 //获取主页数据
-<<<<<<< HEAD
-// export let getHome=(id)=>{
-//     return axios.get(`/home${id}`)
-// };
+
 export let getHome=()=>{
     return axios.get('/home')
 };
 
 
 
-=======
-export function getHome(id){
-    return axios.get(`/home${id}`)
-}
->>>>>>> 25cf44f2c3eeec66a6ad77ef64ca99d6ea474a37
 
 
 
